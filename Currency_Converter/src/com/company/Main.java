04e9ec24a35b63d1bb384a5502b00e25935a7131
Roxanne Reindl <rreindl13@gmail.com>
2021-07-28 21:11:30 -0700

    Basic Currency Converter Code

--- Currency_Converter/src/com/company/Main.java
+++ Currency_Converter/src/com/company/Main.java
@@ -1,13 +1,37 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        double amount, dollar, peso;
+
         System.out.println("Testing");
 
-        //More testing to see if this actually works!
+        //Scanner object created
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the SoDev Currency Converter! \n Enter the amount you wish to convert and see instant results");
 
-        //CJ Test!!!
+
+        System.out.println("Which currency do you wish to convert");
+        System.out.println("1.Dollar\n2.)Peso");
+        double code = sc.nextInt();
+
+        System.out.println("How much money do you want to convert ");
+        amount = sc.nextFloat();
+
+        if (code == 1) {
+            dollar = amount;
+            System.out.println("Your conversion is: " + amount + "Dollars");
+        } else {
+            peso = amount / 0.050;
+            System.out.println(peso);
+        }
+
+
+
     }
 }
